3831acc5fbb9e4d168c78392d2871bdc7f1823a0
domi <d.gonzalez@codebay-innovation.com>
2021-11-22 19:48:23 +0000

    save code

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,49 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }} >
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          HOLA HOAL
-        </Link>
-      </h1>
+const Header = () => (
+  <header>
+    <div className="header-content text-white flex items-center justify-around py-2">
+      <Link to="/">
+        <StaticImage
+          src="../images/logo.png"
+          width={63}
+          quality={100}
+          formats={["auto", "webp", "avif"]}
+          alt="A logo"
+        />
+      </Link>
+      <div>
+        <span className="text-lg mx-6">
+        Inicio
+      </span>
+        <span className="text-lg mx-6">
+        Conocenos
+      </span>
+        <span className="text-lg mx-6">
+        Proveedores
+      </span>
+        <span className="text-lg mx-6">
+        Saentech Pro
+      </span>
+        <span className="text-lg mx-6">
+        Contacto
+      </span>
+      </div>
+      <div className='flex items-center'>
+        <span className="text-secondary text-lg mx-2">
+        +00 123 456 789
+        </span>
+        <div className='bg-primary rounded-full py-1 px-4 mx-2 text-black'>
+          Contáctanos
+        </div>
+        <div className='bg-transparent border-2 border-solid border-primary rounded-full py-1 px-4 mx-2'>
+          Acceso Clientes
+        </div>
+      </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+      primary: '#BDD345',
+      secondary: '#58585A'
+    },
+    fontSize: {
+      'xs': '10px',
+      'sm': '12px',
+      'md': '14px',
+      'lg': '16px',
+      'xl': '18px',
+      '7xl': '48px'
+    }
   },
   variants: {
     extend: {},
