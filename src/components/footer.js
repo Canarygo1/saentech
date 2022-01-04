9bb0f2d80380fb1fd71400e83775534626b5f925
domi <d.gonzalez@codebay-innovation.com>
2022-01-04 08:34:45 +0000

    repair fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 import * as React from "react"
 
 const Footer = () => (
-  <div className="w-full mb-20 text-md text-darkBlue text-opacity-50 flex justify-between px-10">
+  <div className="w-full mb-20 text-md text-darkBlue text-opacity-50 flex flex-col sm:flex-row align-middle justify-between px-10">
     <span>Copyright © 2020 tempy.club</span>
-    <div className='flex justify-between w-1/5'>
+    <div className='flex justify-between w-1/5 flex-col sm:flex-row'>
       <span>Privacy Policy</span>
       <span>Terms of Use</span>
       <span>GDPR</span>
