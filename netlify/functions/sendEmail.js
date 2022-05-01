c07ee11acfe7dd9b63a72d25f6d7345d2879cfd4
Canarygo1 <33827581+Canarygo1@users.noreply.github.com>
2022-05-01 20:25:49 +0100

    feature: mail function

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -2,12 +2,18 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 exports.handler =  async (event, context, callback) => {
+  console.log(event)
+  const data = JSON.parse(event.body)
+  const { subject } = data;
+  const body = Object.keys(data).map((k) => {
+    return `${k}: ${data[k]}`
+  }).join("<br><br>");
 
   const mail_to_send = {
     to: "alecruzdev@gmail.com",
     from: 'contacto.cross.agency@gmail.com',
-    subject: 'TEST01',
-    html: "PRUEBA",
+    subject: subject,
+    html: body,
   };
   try{
     await sgMail.send(mail_to_send)
